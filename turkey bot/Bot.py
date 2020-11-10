0f62e0986bf0103f01c69ad1cd7d5fc99ae0c1bb
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2020-11-11 00:45:25 +0600

    Update Bot.py

--- turkey bot/Bot.py
+++ turkey bot/Bot.py
@@ -100,4 +100,4 @@ async def unBan(ctx,*,member):
 
         await ctx.send(member+" was not found") 
 
-client.run("Nzc0NTM4NTA3MzE0MDAzOTc5.X6ZPMg.7AVPEiO_si1LReHk4oYGviCxxMg")
+client.run("Nzc0NTM4NTA3MzE0MDAzOTc5.X6ZPMg.MS4bbLnG4l_7xjlKHrI0UYtq8n8")
