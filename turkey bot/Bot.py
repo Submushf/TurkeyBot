f5a83a85556db0fafe3c56678e04e567a5223006
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2020-11-15 20:16:09 +0600

    Update Bot.py

--- turkey bot/Bot.py
+++ turkey bot/Bot.py
@@ -106,9 +106,9 @@ async def giveaway(ctx):
 @client.command()
 async def help(ctx):
     embed = discord.Embed(title = "Commands" , color = 0xFF8000) 
-    embed.add_field(name= "🏓 ping" , value= "-reply's pong! . " , inline= False)
-    embed.add_field(name= "📒 minfo" , value= "-shows info about the mentioned user . " , inline= False) 
-    embed.add_field(name= "🎉 giveaway" , value= "-start a giveaway. required role (Giveaway hoster) " , inline= False) 
+    embed.add_field(name= "🏓 ping" , value= "-reply's pong! . " , inline= True)
+    embed.add_field(name= "📒 minfo" , value= "-shows info about the mentioned user . " , inline= True) 
+    embed.add_field(name= "🎉 giveaway" , value= "-start a giveaway. required role (Giveaway hoster) " , inline= True) 
     embed.add_field(name= ".", value= ".", inline = True)
     embed.add_field(name= "📊 balance " , value= "-Check your account balance " , inline= False)
     embed.add_field(name= "🤏 beg " , value= "-Beg for money " , inline= True)    
