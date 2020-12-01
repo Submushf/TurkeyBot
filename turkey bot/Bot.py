0019d0dfd3cc77fa6525d9f340ab5ed88636ee74
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2020-12-01 23:48:07 +0600

    Update Bot.py

--- turkey bot/Bot.py
+++ turkey bot/Bot.py
@@ -12,7 +12,7 @@ client.remove_command("help")
 @client.event
 async def on_ready():
     await client.change_presence(status = discord.Status.online, activity= discord.Activity(
-        type= discord.ActivityType.watching, name= "t!help" 
+        type= discord.ActivityType.watching, name= "❄️The snow fall" 
     )) 
     print("bot is ready")
 
