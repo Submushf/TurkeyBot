4e82052208f640d690e90ff0b4e6b75b28f574f7
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2020-12-11 13:15:05 +0600

    update

--- turkey bot/Bot.py
+++ turkey bot/Bot.py
@@ -6,7 +6,7 @@ import asyncio
 import json
 import os
 
-client = commands.Bot(command_prefix= "t!", intents = discord.Intents.all())  
+client = commands.Bot(command_prefix= "g!", intents = discord.Intents.all())  
 client.remove_command("help")
 
 @client.event
