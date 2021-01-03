442d4edeeec596ffaade7efe761b2bd56fd7927a
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-01-03 18:30:36 +0600

    update

--- turkey bot/cogs/A help.py
+++ turkey bot/cogs/A help.py
@@ -15,7 +15,7 @@ class Help(commands.Cog):
     )
     async def help(self, ctx, cog='1'):
         helpembed= discord.Embed(
-            title= "Help Command" , color = 0x07C9F5 
+            title= "Help" , color = 0x07C9F5 
         )
         helpembed.set_thumbnail(url= "https://cdn.discordapp.com/attachments/781888173286227970/786850459317960714/ezgif.com-gif-maker.png") 
 
--- turkey bot/cogs/C Fun.py
+++ turkey bot/cogs/C Fun.py
@@ -1,4 +1,5 @@
 import discord 
+from aiohttp import ClientSession
 from discord.ext import commands
 import random
 
@@ -7,21 +8,24 @@ class Fun(commands.Cog):
     def __init__(self, client):
         self.client = client 
 
-    @commands.command(description = "Reply's pong!")
-    async def ping(self, ctx):
-        await ctx.send("Pong!")
+    @commands.command(
+        name="dadjoke",
+        description="Send a dad joke!",
+        aliases=['dadjokes']
+    )
+    async def dadjoke(self, ctx):
+        url = "https://dad-jokes.p.rapidapi.com/random/jokes"
 
-    @commands.command(description = "Slap a user with a Gif")
-    async def slap(self, ctx):
-        embed = discord.Embed(color = 0x07C9F5 )
-        embed.set_image(url= 'https://media.giphy.com/media/xUNd9HZq1itMkiK652/giphy.gif')
-        await ctx.send(embed=embed)
+        headers = {
+            'x-rapidapi-key': "81dd963d15mshf3e3a91dd6fe3cap1d971djsnbeb11a691831",
+            'x-rapidapi-host': "dad-jokes.p.rapidapi.com" 
+        }
 
-    @commands.command(description = "Beat a user with a Gif") 
-    async def beat(self, ctx):
-        embed = discord.Embed(color = 0x07C9F5 )
-        embed.set_image(url= 'https://media.giphy.com/media/m6etefcEsTANa/giphy.gif')
-        await ctx.send(embed=embed)
+        async with ClientSession() as session:
+            async with session.get(url, headers=headers) as response:
+                r = await response.json()
+                r = r["body"][0]
+                await ctx.send(f"**{r['setup']}**\n||{r['punchline']}||")
 
     @commands.command(description = "Pat a user with a Gif")
     async def pat(self, ctx):
@@ -41,17 +45,25 @@ class Fun(commands.Cog):
         embed.set_image(url= 'https://media.giphy.com/media/u2LJ0n4lx6jF6/giphy.gif')
         await ctx.send(embed=embed)
 
-    @commands.command(description = "Baka a user with an Gif")
-    async def baka(self, ctx):
-        embed = discord.Embed(color = 0x07C9F5)
-        embed.set_image(url= 'https://media.giphy.com/media/4QxQgWZHbeYwM/giphy.gif')
-        await ctx.send(embed=embed)
+    @commands.command(aliases=['s']) 
+    @commands.cooldown(1, 43200, commands.BucketType.user)
+    async def suggest(self,ctx):
+        suggested =  random.choice(cmd)
 
-    @commands.command(description = "Uno reverse")
-    async def reverse(self, ctx):
-        embed = discord.Embed(color = 0xF59307)
-        embed.set_image(url= 'https://media.giphy.com/media/Wt6kNaMjofj1jHkF7t/giphy.gif') 
-        await ctx.send(embed=embed)
+        await ctx.send(f"**Today's suggestion's : {suggested}**")
+        await ctx.send(f"Next suggestion available in 12 hours") 
+
+
+cmd = [
+    "https://www.youtube.com/watch?v=Zszs7Jftv-M","https://www.youtube.com/watch?v=JBDt6ddgsNU","https://www.youtube.com/watch?v=qMmhnOQx7QE","https://www.youtube.com/watch?v=K8oAnM6p2HU&t=170s","https://www.youtube.com/watch?v=CUJFROAW5yA&list=RDCUJFROAW5yA&start_radio=1","https://www.youtube.com/watch?v=7xnW42MeCEc",
+    "https://www.youtube.com/watch?v=lLvoZ5Wsf94","","https://youtu.be/3z4Hzo9UmrQ","https://www.youtube.com/watch?v=qLACUEjyHjw", "https://youtu.be/c6D8v6DhKc4", "https://youtu.be/2nVPFdqejD0", "https://youtu.be/lhSjYT7pWkw"
+    ,"https://youtu.be/2nVPFdqejD0","https://www.youtube.com/watch?v=vdRYJPnBGVU","https://www.youtube.com/watch?v=DODLEX4zzLQ","https://www.youtube.com/watch?v=sKDzYQuPBsY","https://www.digitaltrends.com/web/funniest-youtube-videos/","https://www.youtube.com/watch?v=4d7XuZg5gkk","https://www.youtube.com/watch?v=QycJiYBPNIU","https://www.youtube.com/watch?v=RNFpMDXTCU8",
+    "https://www.youtube.com/watch?v=9DWDJmC1Pkw","https://www.youtube.com/watch?v=VQJ40f_JdOI","https://www.youtube.com/watch?v=OqT8QK84KBc","https://www.youtube.com/watch?v=x-brv0EaPuE","https://www.youtube.com/watch?v=Zo_Y-n__Cbc",
+    "https://www.youtube.com/watch?v=DrwYZCN2__g","https://www.youtube.com/watch?v=06kqZVHCSIs","https://www.youtube.com/watch?v=8oU0LAKoAL0","https://www.youtube.com/watch?v=J5Rzr-GL50Y","https://www.gq.com/story/no-cry-challenge-sad-internet-videos",
+    "https://www.youtube.com/watch?v=tlvO3LnPQR8","https://www.youtube.com/watch?v=GfFVJyDVZuQ&list=WL&index=22&t=112s","https://www.youtube.com/watch?v=f0j--Y6G4Fg&list=WL&index=21&t=10s", "https://www.youtube.com/watch?v=Kv-OdJLhfFI","https://www.youtube.com/watch?v=Kv-OdJLhfFI","https://www.youtube.com/watch?v=OYMJNDB-rx0","https://www.youtube.com/watch?v=fxO4DCEw1FY",
+    "https://www.youtube.com/watch?v=ftsBRXe_lhs","https://www.youtube.com/watch?v=HJjkw1kIgY0","https://www.youtube.com/watch?v=zBG8wsX3NkM"
+]
+
 
 def setup(client):
     client.add_cog(Fun(client))
