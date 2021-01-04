97c86311ae34da842e742aceab10f7ac3fefd339
Tonys Jarvis <72116939+Submushf@users.noreply.github.com>
2021-01-04 11:47:12 +0600

    Update A help.py

--- turkey bot/cogs/A help.py
+++ turkey bot/cogs/A help.py
@@ -17,7 +17,7 @@ class Help(commands.Cog):
         helpembed= discord.Embed(
             title= "Help" , color = 0x07C9F5 
         )
-        helpembed.set_thumbnail(url= "https://cdn.discordapp.com/attachments/781888173286227970/786850459317960714/ezgif.com-gif-maker.png") 
+        helpembed.set_thumbnail(url= "https://cdn.discordapp.com/attachments/781887772390719498/795528385329102948/Capture.PNG") 
 
         cogs = [c for c in self.client.cogs.keys()]
 
